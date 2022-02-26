47f6c849eef42fc4a7083ea01a069dce68b64f85
Yasir Kara <yasir.kara@hotmail.com>
2022-02-26 17:53:32 +0300

    offers page and listing item bug fix

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,10 +4,15 @@ import bedIcon from "../assets/svg/bedIcon.svg";
 import bathtubIcon from "../assets/svg/bathtubIcon.svg";
 
 function ListingItem({ listing, id, onDelete }) {
+
   return (
     <li className="categoryListing">
       <Link to={`/category/${listing.type}/${id}`} className="categoryListingLink">
-        <img src={listing.imgUrls[0]} alt={listing.name} className="categoryListingImg" />
+        <img 
+        src={listing.imageUrls[0]} 
+        alt={listing.name} 
+        className="categoryListingImg" />
+
         <div className="categoryListingDetails">
           <p className="categoryListingLocation">{listing.location}</p>
           <p className="categoryListingName">{listing.name}</p>
