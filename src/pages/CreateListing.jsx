3e37ae6078ed8c7e4467b9c6b50a369ca223240b
Yasir Kara <yasir.kara@hotmail.com>
2022-02-27 09:16:23 +0300

    google geolocation api

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
-
+import { toast } from "react-toastify"
 function CreateListing() {
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [geolocationEnabled, setGeolocationEnabled] = useState(false);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
@@ -59,8 +59,45 @@ function CreateListing() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted]);
 
-  const onSubmit = (e) => {
+  const onSubmit =  async (e) => {
     e.preventDefault();
+    setLoading(true)
+
+    if(discountedPrice >= regularPrice) {
+      setLoading(false)
+      toast.error("Discounted price needs to be less than regular price");
+      return
+    }
+    if(images.length > 6){
+      setLoading(false)
+      toast.error("Max 6 images");
+      return
+    }
+
+    let geolocation = {}
+    let location 
+
+    if(geolocationEnabled){
+      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
+      const data = await response.json();
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+      location = data.status === "ZERO_RESULTS" ? undefined : data.results[0]?.formatted_address
+
+      if(location === undefined || location.includes("undefined")){
+        setLoading(false);
+        toast.error("Please enter a correct address")
+        return
+      }
+    }else{
+      geolocation.lat = latitude
+      geolocation.lng = longitude
+      location = address
+      console.log(geolocation,location);
+    }
+
+    setLoading(false);
   };
 
   const onMutate = (e) => {
